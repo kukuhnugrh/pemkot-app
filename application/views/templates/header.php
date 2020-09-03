7e4d17095c3fc950607badf667c4cfc2eaad248f
laura <laurensia.esmeralda@.ti.ukdw.ac.id>
2020-09-03 19:07:32 +0700

    Beranda (new)

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,20 +17,20 @@
 
   <nav class="navbar navbar-expand-lg navbar-light" >
     <div class="container">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="#" >
         <img src="assets/img/Logo_Kota_Yogyakarta.png" style="width:5%">
         Kota Yogyakarta
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse" id="navbarNavDropdown" >
+        <ul class="navbar-nav" >
           <li class="nav-item active">
-            <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#" >Beranda <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
               Fasilitas
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
