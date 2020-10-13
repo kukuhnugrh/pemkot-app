2b5550143ebc460b9c1648e5b7d3eacd4a29d182
aprillianurs <70563984+aprillianurs@users.noreply.github.com>
2020-10-13 13:46:13 +0700

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,44 @@
+<footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="row align-items-stretch mb-5">
+            <div class="col-md-6">
+                <div class="col-lg-6 text-left"><img src="assets/img/browser.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                <div class="col-lg-6 text-left"><img src="assets/img/phone.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                <div class="col-lg-6 text-left"><img src="assets/img/email.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                    
+            </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group mb-md-0">
+                        <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group form-group-textarea mb-md-0">
+                        <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col text-center">Copyright © Your Website 2020</div>
+            </div>
+        </div>
+        
+    </footer>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
