d92d1d274655187e8db9eda0127deaaa8e80ddfe
regitaard <63082495+regitaard@users.noreply.github.com>
2020-10-13 21:40:45 +0700

    Add files via upload

---
+++ qna.php
@@ -0,0 +1,254 @@
+<header class="masthead">
+    <div class="container">
+      <div class="masthead-heading text-uppercase">Frequently Asked Question</div></br>
+       <div class="masthead-heading text-uppercase"><b>_______________________</b></div>
+    </div>
+</header>
+
+<section class="ftco-intro">
+    <div class="text-center">
+    					<div class="text">
+    						<h5>Pertanyaan yang sering muncul (frequently asked questions, FAQ) adalah halaman untuk menampung dan menyajikan berbagai pertanyaan yang sering muncul dalam proses perencanaan maupun pelaksanaan kerja sama daerah (KSD). Daftar pertanyaan akan terus ditambah seiring berjalannya waktu.</h5>
+                <h1><b>* * *</b></h1>
+              </div>
+    	</div>
+</section>
+
+<div class="container">
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+  <div class="row">
+    <div class="col-6 col-md-4"></br>
+    <h5><b>Ada yang bisa kami bantu?</b></h5>
+    <form class="form-inline ">
+      <input class="form-control form-control-sm mr-4 w-75" type="text" placeholder="Search"
+        aria-label="Search">
+      <i class="fa fa-search" aria-hidden="true"></i>
+    </form></br>
+    <div class="card bg-light mb-3" style="max-width: 32rem;">
+        <div class="card-header">Kolom search diatas merupakan kolom untuk melakukan pencarian</div>
+        <div class="card-body">
+          <p class="card-text">Jika anda ingin mencari sebuah pertanyaan yang ingin diketahui,silakan masukkan kata kunci pada kolom search,kemudian klik icon search untuk mulai melakukan pencarian</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-8">
+
+ 
+
+<div class="accordion" id="accordionExample"style="margin-top: 40px">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <h5>Bagaimana cara untuk menawarkan kerja sama kepada Pemerintah Kota Yogyakarta?</5>
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+      Pemerintah Kota Yogyakarta dapat bekerjasama dengan organisasi swasta maupun pemerintahan (kementerian/lembaga/badan/pemda lainnya) baik dalam maupun luar negeri. Untuk melaksanakan kerja sama tersebut, seluruh pemerintah daerah di Indonesia harus mengikuti tata cara pelaksanaan kerja sama daerah (KSD) yang diatur dalam Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 22 Tahun 2020 (untuk kerja sama dengan entitas dalam negeri) dan Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 25 Tahun 2020 (untuk kerja sama dengan entitas luar negeri). Untuk mengetahui prosedur dan persyaratan untuk menawarkan kerja sama daerah kepada Pemerintah Kota Yogyakarta, silahkan klik di sini.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link  text-dark" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <h5>Ada tawaran kerja sama daerah dari pihak lain, apa yang harus dilakukan oleh OPD?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+      <b>Langkah pertama</b>, diskusikan dengan pihak yang menawarkan kerja sama daerah (KSD) secara mendalam tentang: objek yang akan dikerjasamakan, ruang lingkup, manfaat dan pembiayaan. Pastikan pula pembagian hak dan kewajiban di antara para pihak sesuai dengan kewenangan masing-masing. <b>Langkah kedua</b>, sampaikan kepada pihak yang menawarkan KSD untuk mengikuti prosedur yang tertera pada menu <b>UNTUK MITRA</b> (klik di sini). Jika diperlukan, silahkan untuk menghubungi Sub Bagian Kerjasama, Bagian P3ADK melalui nomor ekstensi 578.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link  text-dark" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah Kepala OPD dapat langsung melaksanakan penawaran kerja sama daerah kepada pihak lain?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+      Dalam Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 22 Tahun 2020 (<a href="https://drive.google.com/file/d/19ULMldzI5y9rQNOnWbe0hdqDdN0DCE_R/view"><u><b>klik di sini</b></u></a>), <b>penawaran kerja sama daerah</b> dengan daerah lain (KSDD) atau dengan pihak ketiga (KSDPK) atau dengan kementerian/lembaga/badan (pemerintah pusat), <b>dilaksanakan oleh kepala daerah</b>. OPD melakukan perencanaan dan inisiasi KSD yang akan dikaji terlebih dahulu oleh Tim Koordinasi Kerja Sama Daerah (TKKSD) Kota Yogyakarta. Apabila rencana KSD dinilai layak, TKKSD akan menyusun surat penawaran KSD yang akan ditandatangani oleh Walikota Yogyakarta.
+      OPD yang akan melaksanakan penawaran kerja sama daerah, harap mempelajari <i>business process</i> dan mekanisme kerja sama daerah (<a href="https://docs.google.com/presentation/d/1ft1MZsPfaAbEiCtx2DSXOpHUldEyaXfvCburqTUpV0E/edit"><u><b>klik di sini</b></u></a>). Pelaksanaan kerja sama daerah di Pemerintah Kota Yogyakarta dikoordinasikan oleh Sekretaris Daerah selaku Ketua TKKSD.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading4">
+      <h2 class="mb-0">
+        <button class="btn btn-link  text-dark" type="button" data-toggle="collapse" data-target="#collapse4" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Siapakah yang melakukan perancangan dokumen kerja sama (MoU/PKS/lainnya?)</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionExample">
+      <div class="card-body">
+      Terdapat beberapa jenis dokumen kerja sama daerah (KSD) yang diakui dalam <b><u>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 22 Tahun 2020.</u></b> Pihak yang menyusun naskah/dokumen KSD dibedakan menurut jenis dokumennya. Silahkan mempelajari materi KSD untuk mengetahui dokumen apa saja yang dihasilkan dan siapa yang bertanggungjawab untuk menyusunnya (<a href="https://docs.google.com/presentation/d/1ft1MZsPfaAbEiCtx2DSXOpHUldEyaXfvCburqTUpV0E/edit"><u><b>klik di sini</b></u></a>).
+      </div>
+    </div>
+  </div>
+  
+  <div class="card">
+    <div class="card-header" id="heading5">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse5" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah OPD dapat melaksanakan perjanjian kerja sama dengan instansi pemerintah pusat (kementerian/lembaga/badan)?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordionExample">
+      <div class="card-body">
+      <b>Ya, dapat.</b> Pelaksanaan kerja sama antara Pemerintah Kota Yogyakarta dengan instansi pemerintah pusat dilaksanakan dengan menyusun Nota Kesepakatan (<a href="https://drive.google.com/file/d/1grLYr3RdEQJeRQfZtmV5kEmHHMhjqYQA/view"><u><b>klik di sini</b></u></a>) yang harus dilampiri dengan rencana kerja (<a href="https://drive.google.com/file/d/1In2venevDV6ODCSRe1g_0uqpxZerYo_o/view"><u><b>klik di sini</b></u></a>). <b>Tidak ada lagi perjanjian kerja sama</b> untuk melaksanakan kerja sama daerah dengan instansi pemerintah pusat. Dasar hukum yang digunakan paling tidak ada 3 (tiga), yaitu: <b>UU No.23/2014 tentang Pemerintahan Daerah</b> (<a href="https://drive.google.com/file/d/16fdIOCQbuezf52uuP8_IQnfncoTQ-_eJ/view"><u><b>klik di sini</b></u></a>), <b>PP No.28/2018 tentang Kerja Sama Daerah</b> (<a href="https://drive.google.com/file/d/1h5apdj4w5gGENKs5o3IzMxIG7gtX0q4N/view"><u><b>klik di sini</b></u></a>) dan <b>Permendagri No.22/2020 tentang Tata Cara Kerja Sama Daerah dengan Daerah Lain dan Kerja Sama Daerah dengan Pihak Ketiga</b>(<a href="https://drive.google.com/file/d/19ULMldzI5y9rQNOnWbe0hdqDdN0DCE_R/view"><u><b>klik di sini</b></u></a>).
+      </div>
+    </div>
+  </div>
+   
+  <div class="card">
+    <div class="card-header" id="heading6">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse6" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Siapakah yang dapat menandatangani dokumen perjanjian kerja sama?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse6" class="collapse" aria-labelledby="heading6" data-parent="#accordionExample">
+      <div class="card-body">
+      Walikota Yogyakarta pada prinsipnya dapat menandatangani seluruh naskah perjanjian kerja sama daerah, meliputi: Kesepakatan Bersama (KB), Nota Kesepakatan (NK), Perjanjian Kerja Sama (PKS), <i>Letter of Intent</i> (LoI, dengan lembaga asing) dan <i>Memorandum of Understanding</i> (MoU, dengan lembaga asing). Namun demikian, pejabat yang diberikan kuasa oleh Walikota Yogyakarta hanya dapat menandatangani PKS.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading7">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse7" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Berapa tahun jangka waktu maksimal suatu perjanjian kerja sama? dan apakah dapat diperpanjang?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse7" class="collapse" aria-labelledby="heading7" data-parent="#accordionExample">
+      <div class="card-body">
+      Dokumen perjanjian kerja sama daerah dapat berlaku paling lama 5 (lima) tahun dan dapat diperpanjang atas kesepakatan para pihak, kecuali ditentukan lain oleh peraturan perundang-undangan yang mengatur khusus mengenai objek atau tata cara kerja sama tertentu.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading8">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse8" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah hibah daerah termasuk dalam perjanjian kerja sama?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse8" class="collapse" aria-labelledby="heading8" data-parent="#accordionExample">
+      <div class="card-body">
+      <b>Hibah daerah</b> dapat merupakan bagian dari pelaksanaan suatu kerja sama daerah (KSD) dan dapat pula merupakan suatu kegiatan tersendiri yang terlepas dari mekanisme KSD. Penatalaksanaan hibah daerah baik sebagai kegiatan tersendiri maupun sebagai bagian dari pelaksanaan KSD mengikuti <b>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 32 Tahun 2011</b> (<a href="https://drive.google.com/file/d/1D5091r3IUlZb72x6VJ--7zgrVDLpeGe6/view"><u><b>klik di sini</b></u></a>) dan peraturan perubahannya yaitu <b>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 13 Tahun 2018</b> (<a href="https://drive.google.com/file/d/19qrP15QAlFQ-KPQPmKHw4vVUyuwLROOz/view"><u><b>klik di sini</b></u></a>). Untuk pelaksanaan hibah daerah, dapat dikoordinasikan dengan Bidang Anggaran pada Badan Pengelola Keuangan dan Aset Daerah (BPKAD) Kota Yogyakarta.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading9">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse9" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah itu Tim Koordinasi Kerja Sama Daerah (TKKSD)?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse9" class="collapse" aria-labelledby="heading9" data-parent="#accordionExample">
+      <div class="card-body">
+      TKKSD adalah tim yang dibentuk oleh kepala daerah (Walikota Yogyakarta) untuk membantu kepala daerah dalam menyiapkan kerja sama daerah. TKKSD Kota Yogyakarta ditetapkan dengan Keputusan Walikota Yogyakarta Nomor 55 Tahun 2020 (<a href="https://drive.google.com/file/d/18LqYdGX4S5K-n_zgQnCWvJQpX4fwrDgU/view"><u><b>klik di sini</b></u></a>).
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading10">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse10" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah perpanjangan perjanjian kerja sama (PKS) tetap harus mengikuti prosedur kerja sama daerah (KSD) yang baru?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse10" class="collapse" aria-labelledby="heading10" data-parent="#accordionExample">
+      <div class="card-body">
+      Ya. Perpanjangan perjanjian kerja sama (PKS) harus mengikuti prosedur kerja sama daerah (KSD) terbaru yang terdapat dalam website ini. Silahkan mengunjungi menu <b>UNTUK OPD</b> ( klik di sini).
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading11">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse11" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah pemerintah daerah dapat langsung melaksanakan kerja sama dengan lembaga asing?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse11" class="collapse" aria-labelledby="heading11" data-parent="#accordionExample">
+      <div class="card-body">
+      <b>Tidak.</b> Setiap perjanjian kerja sama yang dilakukan dengan pemerintah daerah di luar negeri, atau lembaga internasional atau badan swasta asing harus mendapat persetujuan pemerintah pusat terlebih dahulu setelah disetujui oleh DPRD Kota Yogyakarta.Silahkan merujuk pada <b>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 25 Tahun 2020</b>(<a href="https://drive.google.com/file/d/1CK0Rxq784nozqV06ICIkXBANwb8skt5T/view"><u><b>klik di sini</b></u></a>)
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="heading12">
+      <h2 class="mb-0">
+        <button class="btn btn-link text-dark" type="button" data-toggle="collapse" data-target="#collapse12" aria-expanded="false" aria-controls="collapseThree">
+        <h5>Apakah pihak lain dapat memanfaatkan aset milik Pemerintah Kota Yogyakarta?</h5>
+        </button>
+      </h2>
+    </div>
+    <div id="collapse12" class="collapse" aria-labelledby="heading12" data-parent="#accordionExample">
+      <div class="card-body">
+      Tergantung pada jenis organisasi dan maksud dari pemanfaatan tersebut. Silahkan mempelajari <b>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 19 Tahun 2016</b> (<a href="https://drive.google.com/file/d/1ZlNqiX5cJF2mmWxcWvrwAljVfvVrPh_u/view"><u><b>klik di sini</b></u></a>) atau berkonsultasi dengan Bidang Aset pada Badan Pengelola Keuangan dan Aset Daerah (BPKAD) di nomor ekstensi 244 atau 239. Pengetahuan umum tentang pemanfaatan barang milik daerah (BMD) dapat di akses<a href="https://docs.google.com/presentation/d/1ft1MZsPfaAbEiCtx2DSXOpHUldEyaXfvCburqTUpV0E/edit#slide=id.p"><u><b> di sini</b></u></a>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+  </div>
+  
+</br>
+</br>
+<!-- Footer-->
+<footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="row align-items-stretch mb-5">
+            <div class="col-md-6">
+                <div class="col-lg-6 text-left"><img src="assets/img/browser.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                <div class="col-lg-6 text-left"><img src="assets/img/phone.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                <div class="col-lg-6 text-left"><img src="assets/img/email.png" style="width:5%"> Pemerintah Kota Yogyakarta</div>
+                    
+            </div>
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group mb-md-0">
+                        <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group form-group-textarea mb-md-0">
+                        <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col text-center">Copyright © Your Website 2020</div>
+            </div>
+        </div>
+        
+    </footer>
