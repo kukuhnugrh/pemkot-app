873033d87b4b94e0d0b118f104e6b0ca8ae8a47f
nurkarimah <63086402+nurkarimah@users.noreply.github.com>
2020-10-14 12:32:27 +0700

    Add files via upload

---
+++ application/views/templates/header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title> Pemerintah Kota Yogyakarta </title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#4dffa6">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="assets/img/Logo_Kota_Yogyakarta.png" style="width:5%">
+        Pemerintah Kota Yogyakarta
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php base_url() ?>home">Beranda <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              OPD
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="<?php base_url() ?>opd">Informasi OPD</a>
+              <a class="dropdown-item" href="#">Evaluasi OPD</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php base_url() ?>mitra_kerjasama">Mitra</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Informasi
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Konsultasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main Content -->
+  <div class="main-content">
+    <header class="masthead">
+        <div class="container">
+            <div class="masthead-heading text-uppercase"><?php echo $judul ?></div>
+        </div>
+    </header>
